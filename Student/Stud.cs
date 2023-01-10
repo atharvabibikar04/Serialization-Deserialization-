3f99b95d8b4db285b55541cebd63eec8f2cdc3f8
dac <dac@dypm.org>
2023-01-10 16:54:20 +0530

    basic serialization

---
+++ Student/Stud.cs
@@ -0,0 +1,34 @@
+﻿namespace Student;
+// using System.Text.Json;
+// using System.Text.Json.Serialization;
+ [Serializable]
+public class Stud
+{
+public int Id{get;set;}
+public  string Name{get;set;}
+
+public double Marks{get;set;}
+
+public bool Pass{get;set;}
+
+
+//default constructor
+public Stud(){
+    this.Id=1;
+    this.Name="NA";
+    this.Marks=0.0;
+    this.Pass=true;
+}
+
+public Stud(int Id,string Name,double Marks,bool Pass){
+    this.Id=Id;
+    this.Name=Name;
+    this.Marks=Marks;
+    this.Pass=Pass;
+}
+
+    public override string ToString()
+    {
+        return ("Id- "+Id+" Name- "+Name+" Marks- "+Marks+ " Pass- "+Pass);
+    }
+}
---
+++ Tester/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Student;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+List<Stud> li =new List<Stud>();
+Stud s1=new Stud(1,"Atharva",80.0,true);
+Stud s2=new Stud(2,"Rohit",80.0,true);
+Stud s3=new Stud(3,"Akshay",80.0,true);
+
+li.Add(s1);
+li.Add(s2);
+li.Add(s3);
+
+
+foreach (Stud s in li)
+{
+    Console.WriteLine(s);
+}
+
+
+//serialization code
+//  var option =new JsonSerializerOptions{IncludeFields=true};
+// var LoginJson=JsonSerializer.Serialize<List<Stud>>(li,option);
+string fileName=@"E:\Atharva and Raju\DOT NET\Practice\Assignment\login.json";
+// // Console.WriteLine(LoginJson);
+// File.WriteAllText(fileName,LoginJson);
+
+//deserialization
+string Sjson=File.ReadAllText(fileName);
+var LogoutJson=JsonSerializer.Deserialize<List<Stud>>(Sjson);
+
+foreach (Stud s in LogoutJson)
+{
+    Console.WriteLine(s);
+}
+
